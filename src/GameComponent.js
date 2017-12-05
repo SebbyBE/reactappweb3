b0383835ff4b359e0ca99d344479522f5d7ffe85
SebbyBe <sebastien.place@yahoo.fr>
2017-12-05 01:08:07 +0100

    add script element

--- src/GameComponent.js
+++ src/GameComponent.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Script from 'react-load-script';
 
 class GameComponent extends Component{
     render(){
@@ -8,6 +9,7 @@ class GameComponent extends Component{
             <div className="col-xs-12 col-sm-12 col-md-10" id="gameDiv">
             </div>
             <div className="col-xs-0 col-sm-0 col-md-1" id="right_pannel"></div>
+            <Script url={process.env.PUBLIC_URL + "/js/main.js"}/>
             </div>
         );
     }
